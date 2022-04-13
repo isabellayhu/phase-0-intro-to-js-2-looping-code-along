05115e73254c332ff2066323962eb343f096cb17
Isabella <isabella.y.hu@gmail.com>
2022-04-13 21:46:25 +1000

    commit tree 0d4597bfb237262caa9b71a6d80118d37e3694d9

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const names=[];
+function writeCards(names,type){
+    const messages = []
+    for (let i=0; i<names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`)
+    }
+    return messages
+}
+
+function countDown(i){
+    while (i>=0){
+        console.log(i--);
+    }
+}
